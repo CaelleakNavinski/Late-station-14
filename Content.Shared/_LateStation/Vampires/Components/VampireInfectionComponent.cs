bec84d93cf13e57c49612832ad5fb4fc3d42c677
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-04-28 12:35:02 -0500

    Update VampireInfectionComponent.cs
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Components/VampireInfectionComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/VampireInfectionComponent.cs
@@ -1,24 +1,17 @@
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
+using Robust.Shared.Serialization;
 
-namespace Content.Shared.Vampire.Components
+namespace Content.Shared._LateStation.Vampires.Components
 {
-    /// <summary>
-    /// Tracks a pending vampire conversion and syncs its countdown to all clients.
-    /// </summary>
-    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+    [RegisterComponent]
+    [NetworkedComponent]
+    [AutoGenerateComponentState]
     public sealed partial class VampireInfectionComponent : Component
     {
-        /// <summary>
-        /// Remaining time (in seconds) until the victim flips to full vampirism.
-        /// </summary>
         [ViewVariables(VVAccess.ReadWrite), DataField("timeLeft"), AutoNetworkedField]
         public float TimeLeft { get; set; } = 120f;
 
-        /// <summary>
-        /// Upper limit for how long conversion can be delayed (in seconds).
-        /// </summary>
         [ViewVariables(VVAccess.ReadWrite), DataField("maxTime"), AutoNetworkedField]
         public float MaxTime { get; set; } = 180f;
     }
