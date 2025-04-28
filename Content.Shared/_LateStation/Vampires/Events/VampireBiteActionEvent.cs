276b30f65b2226484c084953a3b4c6d3fc54a820
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-04-28 12:57:03 -0500

    I REALLY HATE BUILD ERRORS
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Events/VampireBiteActionEvent.cs
+++ Content.Shared/_LateStation/Vampires/Events/VampireBiteActionEvent.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Actions;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Prototypes;   // ← add this so [Prototype] resolves
 
 namespace Content.Shared._LateStation.Vampires.Events
 {
@@ -8,8 +9,8 @@ namespace Content.Shared._LateStation.Vampires.Events
     public sealed partial class VampireBiteActionEvent : EntityTargetActionEvent
     {
         [DataField("popupText")]
-        public string PopupText { get; private set; } =
-            "A crimson spray bursts from {Victim} as fangs sink in!";
+        public string PopupText { get; private set; }
+            = "A crimson spray bursts from {Victim} as fangs sink in!";
 
         [DataField("speech")]
         public string? Speech { get; private set; }
