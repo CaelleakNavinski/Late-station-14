a72196bfeee026e4f5ff1cc274fcefbecb9df43a
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-04-28 12:56:04 -0500

    I HATE BUILD ERRORS
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Components/VampireComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/VampireComponent.cs
@@ -1,10 +1,11 @@
 using Content.Shared.StatusIcon;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
+using Robust.Shared.Prototypes;                         // ← add this
 using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
 using Robust.Shared.GameObjects;
+using Content.Shared._LateStation.Vampires.Systems;      // ← add this so Access finds SharedVampireSystem
 
 namespace Content.Shared._LateStation.Vampires.Components
 {
@@ -18,7 +19,8 @@ namespace Content.Shared._LateStation.Vampires.Components
         public ProtoId<FactionIconPrototype> StatusIcon { get; set; } = "VampireFaction";
 
         [DataField("vampStartSound")]
-        public SoundSpecifier VampireStartSound { get; set; } = new SoundPathSpecifier("/Audio/Antag/vampire_start.ogg");
+        public SoundSpecifier VampireStartSound { get; set; }
+            = new SoundPathSpecifier("/Audio/Antag/vampire_start.ogg");
 
         public override bool SessionSpecific => true;
     }
