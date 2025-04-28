f4397282842c054c1d44b8a625aedcc2e155e40c
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-04-28 12:40:35 -0500

    Update VampireInfectionComponent.cs
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Components/VampireInfectionComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/VampireInfectionComponent.cs
@@ -1,6 +1,7 @@
 using Robust.Shared.GameStates;
-using Robust.Shared.ViewVariables;
 using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+using Robust.Shared.GameObjects;
 
 namespace Content.Shared._LateStation.Vampires.Components
 {
@@ -9,10 +10,14 @@ namespace Content.Shared._LateStation.Vampires.Components
     [AutoGenerateComponentState]
     public sealed partial class VampireInfectionComponent : Component
     {
-        [ViewVariables(VVAccess.ReadWrite), DataField("timeLeft"), AutoNetworkedField]
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("timeLeft")]
+        [AutoNetworkedField]
         public float TimeLeft { get; set; } = 120f;
 
-        [ViewVariables(VVAccess.ReadWrite), DataField("maxTime"), AutoNetworkedField]
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("maxTime")]
+        [AutoNetworkedField]
         public float MaxTime { get; set; } = 180f;
     }
 }
