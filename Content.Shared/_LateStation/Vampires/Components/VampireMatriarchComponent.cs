5e06f8db78b20eb442813f078bd1d406b29e26b6
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-05-05 14:20:43 -0500

    guh x2
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Components/VampireMatriarchComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/VampireMatriarchComponent.cs
@@ -1,25 +1,21 @@
 using Robust.Shared.GameStates;
-using Robust.Shared.GameObjects;
+using Robust.Shared.ViewVariables;
 using Content.Shared._LateStation.Vampires.Systems;
-
 
 namespace Content.Shared._LateStation.Vampires.Components
 {
-    /// <summary>
-    /// Marker for the Matriarch role; holds her pre‐upgrade health values.
-    /// </summary>
     [RegisterComponent]
     [NetworkedComponent]
-    // Only the shared sync system needs read/write here:
+    [AutoGenerateComponentState]
     [Access(typeof(SharedVampireSystem))]
     public sealed partial class VampireMatriarchComponent : Component
     {
-        /// <summary>Stored MaxHP before Matriarch buff.</summary>
-        [ViewVariables(VVAccess.ReadOnly)]
+        [ViewVariables(VVAccess.ReadWrite)]
+        [AutoNetworkedField]
         public float OriginalMaxHP { get; set; }
 
-        /// <summary>Stored crit threshold before Matriarch buff.</summary>
-        [ViewVariables(VVAccess.ReadOnly)]
+        [ViewVariables(VVAccess.ReadWrite)]
+        [AutoNetworkedField]
         public float OriginalCritThreshold { get; set; }
     }
 }
