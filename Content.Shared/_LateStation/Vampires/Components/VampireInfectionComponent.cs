eea29365c11b50d952c155f060986b416a8cf202
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-05-05 14:17:49 -0500

    guh
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Components/VampireInfectionComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/VampireInfectionComponent.cs
@@ -1,7 +1,6 @@
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
-using Robust.Shared.GameObjects;
 using Content.Shared._LateStation.Vampires.Systems;
 
 namespace Content.Shared._LateStation.Vampires.Components
@@ -9,38 +8,27 @@ namespace Content.Shared._LateStation.Vampires.Components
     [RegisterComponent]
     [NetworkedComponent]
     [AutoGenerateComponentState]
-    // Only the shared system needs read/write here
     [Access(typeof(SharedVampireSystem))]
     public sealed partial class VampireInfectionComponent : Component
     {
-        /// <summary>Seconds remaining until full conversion.</summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("timeLeft")]
-        [AutoNetworkedField]
+        [DataField("timeLeft"), AutoNetworkedField]
         public float TimeLeft { get; set; } = 120f;
 
-        /// <summary>Maximum possible conversion time.</summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("maxTime")]
-        [AutoNetworkedField]
+        [DataField("maxTime"), AutoNetworkedField]
         public float MaxTime { get; set; } = 180f;
 
-        /// <summary>Accumulator for whisper‐popup timing.</summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("popupAccumulator")]
-        [AutoNetworkedField]
+        [DataField("popupAccumulator"), AutoNetworkedField]
         public float PopupAccumulator { get; set; }
 
-        /// <summary>Stores previous tick’s TimeLeft for threshold checks.</summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("previousTimeLeft")]
-        [AutoNetworkedField]
+        [DataField("previousTimeLeft"), AutoNetworkedField]
         public float PreviousTimeLeft { get; set; }
 
-        /// <summary>Index of the last “final stage” message shown.</summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("finalStage")]
-        [AutoNetworkedField]
+        [DataField("finalStage"), AutoNetworkedField]
         public int FinalStage { get; set; }
     }
 }
