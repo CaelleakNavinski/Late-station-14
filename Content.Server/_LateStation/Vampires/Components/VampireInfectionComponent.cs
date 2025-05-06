b19e1a1066405feb268e8925fd6899c4c8ef359b
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-05-06 09:16:24 -0500

    Update VampireInfectionComponent.cs
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Server/_LateStation/Vampires/Components/VampireInfectionComponent.cs
+++ Content.Server/_LateStation/Vampires/Components/VampireInfectionComponent.cs
@@ -3,20 +3,14 @@ using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
 using Robust.Shared.GameObjects;
 using Content.Shared._LateStation.Vampires.Components;
-using Content.Shared._LateStation.Vampires.Systems;
 using Content.Server._LateStation.Vampires.Systems;
 
 namespace Content.Server._LateStation.Vampires.Components
 {
-    [RegisterComponent]
-    [NetworkedComponent]
-    [AutoGenerateComponentState]
-    // Grant both the shared sync system and server‐side VampireInfectionSystem write access
+    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
     [Access(typeof(SharedVampireSystem), typeof(VampireInfectionSystem))]
-    public sealed partial class VampireInfectionComponent : Component
-    {
-        // All properties (TimeLeft, PopupAccumulator, PreviousTimeLeft, FinalStage, etc.)
-        // are declared in the shared partial.  This server partial exists solely to
-        // attach the correct [Access] attribute in the server assembly.
+    public sealed partial class VampireInfectionComponent : Component 
+    { 
+        /// add later
     }
 }
