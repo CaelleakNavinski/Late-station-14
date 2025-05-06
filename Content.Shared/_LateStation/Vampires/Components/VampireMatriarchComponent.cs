a9ca45059cd33c9b33e73bee96ada671fe7e538d
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-05-06 09:14:38 -0500

    Update VampireMatriarchComponent.cs
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Shared/_LateStation/Vampires/Components/VampireMatriarchComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/VampireMatriarchComponent.cs
@@ -1,6 +1,5 @@
 using Robust.Shared.GameStates;
 using Robust.Shared.ViewVariables;
-using Content.Shared._LateStation.Vampires.Systems;
 
 namespace Content.Shared._LateStation.Vampires.Components
 {
@@ -11,11 +10,9 @@ namespace Content.Shared._LateStation.Vampires.Components
     public sealed partial class VampireMatriarchComponent : Component
     {
         [ViewVariables(VVAccess.ReadWrite)]
-        [AutoNetworkedField]
         public float OriginalMaxHP { get; set; }
 
         [ViewVariables(VVAccess.ReadWrite)]
-        [AutoNetworkedField]
         public float OriginalCritThreshold { get; set; }
     }
 }
