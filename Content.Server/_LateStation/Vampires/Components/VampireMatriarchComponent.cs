702f7eb11966d6fcfaaa3de53b647eaf2c2b7f7c
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-05-06 09:16:52 -0500

    Update VampireMatriarchComponent.cs
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Server/_LateStation/Vampires/Components/VampireMatriarchComponent.cs
+++ Content.Server/_LateStation/Vampires/Components/VampireMatriarchComponent.cs
@@ -2,19 +2,14 @@ using Robust.Shared.GameStates;
 using Robust.Shared.ViewVariables;
 using Robust.Shared.GameObjects;
 using Content.Shared._LateStation.Vampires.Components;
-using Content.Shared._LateStation.Vampires.Systems;
 using Content.Server._LateStation.Vampires.Systems;
 
 namespace Content.Server._LateStation.Vampires.Components
 {
-    [RegisterComponent]
-    [NetworkedComponent]
-    [AutoGenerateComponentState]
-    // Grant both SharedVampireSystem (for sync) and VampireRoleSystem (for HP buff/restore) write access
+    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
     [Access(typeof(SharedVampireSystem), typeof(VampireRoleSystem))]
-    public sealed partial class VampireMatriarchComponent : Component
-    {
-        // The two fields/properties are declared in the shared partial.
-        // This server partial only applies the [Access] attribute.
+    public sealed partial class VampireMatriarchComponent : Component 
+    { 
+        /// add later
     }
 }
