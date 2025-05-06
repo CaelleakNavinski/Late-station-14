663d9d1f453f75c26c5a5714af66779b765d6486
Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>
2025-05-06 12:12:44 -0500

    Update VampireClientSystem.cs
    Signed-off-by: Caelleak <154922354+CaelleakNavinski@users.noreply.github.com>

--- Content.Client/_LateStation/Vampires/Systems/VampireClientSystem.cs
+++ Content.Client/_LateStation/Vampires/Systems/VampireClientSystem.cs
@@ -5,15 +5,15 @@ using Content.Shared._LateStation.Vampires.Components;
 namespace Content.Client._LateStation.Vampires.Systems
 {
     public sealed class VampireClientSystem : EntitySystem
-    {
-        public override void Initialize()
-        {
-            SubscribeLocalEvent<VampireInfectionComponent, ComponentGetState>(OnGetState);
-        }
+    //{
+       // public override void Initialize()
+        //{
+            //SubscribeLocalEvent<VampireInfectionComponent, ComponentGetState>(OnGetState);
+        //}
 
-        private void OnGetState(EntityUid uid, VampireInfectionComponent comp, ref ComponentGetState args)
-        {
+        //private void OnGetState(EntityUid uid, VampireInfectionComponent comp, ref ComponentGetState args)
+        //{
             // Client-side vision HUD effects go here.
-        }
-    }
+        //}
+    //}
 }
