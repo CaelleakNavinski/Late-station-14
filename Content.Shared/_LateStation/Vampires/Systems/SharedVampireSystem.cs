b14e4deedb5bd46ef74bf9b40e742f40549497ca
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 00:33:09 -0500

    Update SharedVampireSystem.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

--- Content.Shared/_LateStation/Vampires/Systems/SharedVampireSystem.cs
+++ Content.Shared/_LateStation/Vampires/Systems/SharedVampireSystem.cs
@@ -1,23 +1,38 @@
+using System;
 using Content.Shared.Actions;
-using Robust.Shared.GameStates;
+using Content.Shared.Popups;
+using Content.Shared.StatusIcon.Components;
 using Robust.Shared.GameObjects;
-using Content.Shared._LateStation.Vampires.Components;
+using Robust.Shared.Player;
+using Robust.Shared.Timing;
+using Robust.Shared.IoC;
 
 namespace Content.Shared._LateStation.Vampires.Systems
 {
     /// <summary>
-    /// Shared stub so that [Access(typeof(SharedVampireSystem))] resolves
-    /// and so that both client and server wire up the bite action toggle.
+    /// Shared logic for vampire components: action hookup, state synchronization,
+    /// and client visibility for vampire status icons via GetStatusIconsEvent.
     /// </summary>
     public sealed class SharedVampireSystem : EntitySystem
     {
         [Dependency] private readonly SharedActionsSystem _actions = default!;
+        [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
 
         public override void Initialize()
         {
             base.Initialize();
+
+            // Hook up the bite action
             SubscribeLocalEvent<SharedVampireComponent, ComponentInit>(OnVampireInit);
             SubscribeLocalEvent<SharedVampireComponent, ComponentShutdown>(OnVampireShutdown);
+
+            // Control replication of vampire state to clients
+            SubscribeLocalEvent<SharedVampireComponent, ComponentGetStateAttemptEvent>(OnVampCompGetStateAttempt);
+            SubscribeLocalEvent<SharedVampireMatriarchComponent, ComponentGetStateAttemptEvent>(OnVampCompGetStateAttempt);
+
+            // Ensure new clients get up-to-date vampire info
+            SubscribeLocalEvent<SharedVampireComponent, ComponentStartup>(DirtyVampComps);
+            SubscribeLocalEvent<SharedVampireMatriarchComponent, ComponentStartup>(DirtyVampComps);
         }
 
         private void OnVampireInit(EntityUid uid, SharedVampireComponent comp, ComponentInit args)
@@ -28,6 +43,39 @@ namespace Content.Shared._LateStation.Vampires.Systems
         private void OnVampireShutdown(EntityUid uid, SharedVampireComponent comp, ComponentShutdown args)
         {
             _actions.RemoveAction(uid, comp.BiteActionEntity);
+        }
+
+        private void OnVampCompGetStateAttempt(EntityUid uid, Component comp, ref ComponentGetStateAttemptEvent args)
+        {
+            if (CanGetState(args.Player))
+                return;
+
+            args.Cancelled = true;
+        }
+
+        private bool CanGetState(ICommonSession? player)
+        {
+            if (player?.AttachedEntity is not {} ent)
+                return true;
+
+            // Vampires and Matriarchs always see their own state
+            if (HasComp<SharedVampireComponent>(ent) || HasComp<SharedVampireMatriarchComponent>(ent))
+                return true;
+
+            // Other players see vampire icons only if allowed
+            return HasComp<ShowAntagIconsComponent>(ent);
+        }
+
+        private void DirtyVampComps<T>(EntityUid uid, T comp, ComponentStartup args)
+        {
+            // Force resend of all vampire and matriarch components
+            var vampQuery = AllEntityQuery<SharedVampireComponent>();
+            while (vampQuery.MoveNext(out var id, out var _))
+                Dirty(id);
+
+            var matQuery = AllEntityQuery<SharedVampireMatriarchComponent>();
+            while (matQuery.MoveNext(out var id2, out var _))
+                Dirty(id2);
         }
     }
 }
