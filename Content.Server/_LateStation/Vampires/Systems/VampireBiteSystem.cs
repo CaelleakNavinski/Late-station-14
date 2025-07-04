cb31e16b578dcfbc11212e333a1dc17ba56c4f5f
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 04:27:38 -0500

    Update VampireBiteSystem.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

--- Content.Server/_LateStation/Vampires/Systems/VampireBiteSystem.cs
+++ Content.Server/_LateStation/Vampires/Systems/VampireBiteSystem.cs
@@ -1,13 +1,13 @@
+using Content.Shared.IdentityManagement;                       // Identity.Entity
 using Content.Shared.Popups;
-using Content.Server._LateStation.Vampires.Components;
+using Content.Server._LateStation.Vampires.Components;        // VampireImmuneComponent
 using Content.Shared._LateStation.Vampires.Components;
 using Content.Shared._LateStation.Vampires.Events;
-using Content.Shared.Humanoid;            // for HumanoidComponent
-using Content.Shared.Zombies;
-using Content.Shared.Actions;             // for ActionsComponent (if needed)
-using Robust.Shared.GameStates;           // EntitySystem
-using Robust.Server.GameObjects;          // EntityManager, SharedPopupSystem
-using Robust.Shared.IoC;                  // [Dependency]
+using Content.Shared.Humanoid;                                // HumanoidAppearanceComponent
+using Content.Shared.Zombies;                                 // ZombieComponent
+using Robust.Server.GameObjects;                              // EntityManager
+using Robust.Shared.GameStates;
+using Robust.Shared.IoC;
 using Robust.Shared.Localization;
 
 namespace Content.Server._LateStation.Vampires.Systems
@@ -27,43 +27,45 @@ namespace Content.Server._LateStation.Vampires.Systems
             var user = ev.Performer;
             var target = ev.Target;
 
+            // Only bite humanoids (not zombies or animals)
             if (!HasComp<HumanoidAppearanceComponent>(target) || HasComp<ZombieComponent>(target))
             {
-                 _popup.PopupEntity(
+                _popup.PopupEntity(
                     Loc.GetString("vamp-target-immune-misc-popup", ("victim", Identity.Entity(target))),
                     user,
                     PopupType.SmallCaution);
                 return;
             }
-            
-            // 1) VampireImmuneComponent blocks all bites
+
+            // VampireImmuneComponent blocks all bites
             if (HasComp<VampireImmuneComponent>(target))
             {
                 _popup.PopupEntity(
-                    Loc.GetString("vamp-target-immune-aura-popup", ("victim", Identity.Entity(target))),
+                    Loc.GetString("vamp-target-immune-aura-popup", ("victim", Identity.Entity(target, EntityManager))),
                     user,
                     PopupType.SmallCaution);
                 _popup.PopupEntity(
-                    Loc.GetString("vamp-victim-immune-aura-popup", ("vamp", Identity.Entity(user))),
+                    Loc.GetString("vamp-victim-immune-aura-popup", ("vamp", Identity.Entity(user, EntityManager))),
                     target,
                     PopupType.SmallCaution);
                 return;
             }
 
-            // 2) SharedVampireComponent on target also blocks bites
+            // Already a vampire blocks bites
             if (HasComp<SharedVampireComponent>(target))
             {
                 _popup.PopupEntity(
-                    Loc.GetString("vamp-target-immune-other-vamp-popup", ("victim", Identity.Entity(target))),
+                    Loc.GetString("vamp-target-immune-other-vamp-popup", ("victim", Identity.Entity(target, EntityManager))),
                     user,
                     PopupType.SmallCaution);
                 return;
             }
+
             // Start infection
             EntityManager.AddComponent<VampireInfectionComponent>(target);
 
             // Flavor popup
-            var popup = Loc.GetString("vamp-bite-popup", ("{$victim}", Identity.Entity(target)));
+            var popup = Loc.GetString("vamp-bite-popup", ("{$victim}", Identity.Entity(target, EntityManager)));
             _popup.PopupEntity(popup, target, PopupType.LargeCaution);
 
             EntityManager.RemoveComponent<VampireBiteToggleComponent>(user);
