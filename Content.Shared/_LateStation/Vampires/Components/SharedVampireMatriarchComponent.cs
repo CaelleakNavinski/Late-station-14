83f7aa43d4ebbdc97bd2710726d1c4278ccdf338
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 11:21:54 -0500

    Update SharedVampireMatriarchComponent.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

--- Content.Shared/_LateStation/Vampires/Components/SharedVampireMatriarchComponent.cs
+++ Content.Shared/_LateStation/Vampires/Components/SharedVampireMatriarchComponent.cs
@@ -1,13 +1,21 @@
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
+using Robust.Shared.Prototypes;        // for ProtoId<…>
+using Robust.Shared.Serialization;     
 using Robust.Shared.ViewVariables;
+using Content.Shared.StatusIcon;       // for FactionIconPrototype
+using Content.Shared._LateStation.Vampires.Systems;  // for SharedVampireSystem
 
 namespace Content.Shared._LateStation.Vampires.Components
 {
     [RegisterComponent]
     [NetworkedComponent]
+    [AutoGenerateComponentState] 
+    [Access(typeof(SharedVampireSystem))]
     public sealed partial class SharedVampireMatriarchComponent : Component
     {
-        // marker only; buffs tracked server-side
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("statusIcon")]
+        [AutoNetworkedField]
+        public ProtoId<FactionIconPrototype> StatusIcon { get; set; } = "VampireMatriarchFaction";
     }
 }
