1ea5e6b5b9a0ef426de81217d832e842e1d06e74
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 01:54:36 -0500

    Update VampireRuleComponent.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

--- Content.Server/_LateStation/GameTicking/Rules/Components/VampireRuleComponent.cs
+++ Content.Server/_LateStation/GameTicking/Rules/Components/VampireRuleComponent.cs
@@ -1,23 +1,12 @@
 using Robust.Shared.GameStates;
-using Robust.Shared.GameObjects;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
-using Content.Server.GameTicking.Rules;
+using Content.Server.GameTicking.Rules.Components;
 
-namespace Content.Server._LateStation.GameTicking.Rules.Components
+namespace Content.Server._LateStation.GameTicking.Rules.Components;
+
+[RegisterComponent]
+[Access(typeof(VampireRuleSystem))]
+public sealed class VampireRuleComponent : Component
 {
-    /// <summary>
-    /// Prototype‑backed component for the Vampire round rule.
-    /// Maps to the “VampireRule” entry in roundstart.yml.
-    /// </summary>
-    [RegisterComponent]
-    [ComponentProtoName("VampireRule")]
-    public sealed class VampireRuleComponent : RuleComponent
-    {
-        /// <summary>
-        /// How many Vampire Matriarchs to spawn at round start.
-        /// </summary>
-        [DataField("matriarchCount")]
-        public int MatriarchCount { get; set; } = 1;
-    }
+    [DataField("matriarchCount")]
+    public int MatriarchCount { get; } = 1;
 }
