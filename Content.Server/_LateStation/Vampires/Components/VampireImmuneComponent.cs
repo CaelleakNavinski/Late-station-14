8c46087fb375bbf7cd452fe6d1836cb759548a36
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 04:20:17 -0500

    Create VampireImmuneComponent.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

---
+++ Content.Server/_LateStation/Vampires/Components/VampireImmuneComponent.cs
@@ -0,0 +1,15 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+using Robust.Shared.GameObjects;
+
+namespace Content.Server._LateStation.Vampires.Components
+{
+    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+    [Access(typeof(Content.Shared._LateStation.Vampires.Systems.SharedVampireSystem), typeof(Content.Server._LateStation.Vampires.Systems.VampireBiteSystem))]
+    public sealed partial class VampireImmuneComponent : Component
+    { 
+        
+    }
+}
