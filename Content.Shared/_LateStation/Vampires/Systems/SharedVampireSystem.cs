0e3e0ace10cc8526c355eda31a37f0520afa3948
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 01:26:58 -0500

    Update SharedVampireSystem.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

--- Content.Shared/_LateStation/Vampires/Systems/SharedVampireSystem.cs
+++ Content.Shared/_LateStation/Vampires/Systems/SharedVampireSystem.cs
@@ -2,7 +2,7 @@ using System;
 using Content.Shared.Actions;
 using Content.Shared._LateStation.Vampires.Components;
 using Content.Shared.Popups;
-using Content.Shared.StatusIcon.Components;
+using Content.Shared.Antag;
 using Robust.Shared.GameStates;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Player;
@@ -14,6 +14,7 @@ namespace Content.Shared._LateStation.Vampires.Systems
     /// <summary>
     /// Shared logic for vampire components: action hookup, state synchronization,
     /// and client visibility for vampire status icons via GetStatusIconsEvent.
+    /// Mirrors the pattern in SharedRevolutionarySystem.
     /// </summary>
     public sealed class SharedVampireSystem : EntitySystem
     {
@@ -47,7 +48,7 @@ namespace Content.Shared._LateStation.Vampires.Systems
             _actions.RemoveAction(uid, comp.BiteActionEntity);
         }
 
-        private void OnVampCompGetStateAttempt(EntityUid uid, IComponent comp, ref ComponentGetStateAttemptEvent args)
+        private void OnVampCompGetStateAttempt(EntityUid uid, Component comp, ref ComponentGetStateAttemptEvent args)
         {
             if (CanGetState(args.Player))
                 return;
@@ -57,7 +58,7 @@ namespace Content.Shared._LateStation.Vampires.Systems
 
         private bool CanGetState(ICommonSession? player)
         {
-            if (player?.AttachedEntity is not { } ent)
+            if (player?.AttachedEntity is not {} ent)
                 return true;
 
             // Vampires and Matriarchs always see their own state
@@ -68,17 +69,16 @@ namespace Content.Shared._LateStation.Vampires.Systems
             return HasComp<ShowAntagIconsComponent>(ent);
         }
 
-        private void DirtyVampComps<T>(EntityUid uid, T _, ComponentStartup args) where T : IComponent
+        private void DirtyVampComps<T>(EntityUid uid, T comp, ComponentStartup args)
         {
-            // Force resend of all vampire components
+            // Force resend of all vampire and matriarch components
             var vampQuery = AllEntityQuery<SharedVampireComponent>();
-            while (vampQuery.MoveNext(out var id, out var comp))
-                Dirty(id, comp);
+            while (vampQuery.MoveNext(out var id, out _))
+                Dirty(id);
 
-            // Force resend of all matriarch components
             var matQuery = AllEntityQuery<SharedVampireMatriarchComponent>();
-            while (matQuery.MoveNext(out var id2, out var comp2))
-                Dirty(id2, comp2);
+            while (matQuery.MoveNext(out var id2, out _))
+                Dirty(id2);
         }
     }
 }
