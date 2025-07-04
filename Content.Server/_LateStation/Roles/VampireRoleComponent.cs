188636c3241d88b2ca54e3c0c37b58ca28f42719
CeradonF <cameronfrala.personal@gmail.com>
2025-07-04 00:03:28 -0500

    Create VampireRoleComponent.cs
    Signed-off-by: CeradonF <cameronfrala.personal@gmail.com>

---
+++ Content.Server/_LateStation/Roles/VampireRoleComponent.cs
@@ -0,0 +1,16 @@
+using Content.Shared.Roles;
+
+namespace Content.Server.Roles;
+
+/// <summary>
+///     Added to mind role entities to tag that they are a Revolutionary.
+/// </summary>
+[RegisterComponent]
+public sealed partial class VampireRoleComponent : BaseMindRoleComponent
+{
+    /// <summary>
+    /// For vampmats, how many people you have converted.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public uint ConvertedCount = 0;
+}
