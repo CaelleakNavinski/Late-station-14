927fd6fd8db8110b4bce525517bb34cfa6bf43fa
Frala <cameronfrala.personal@gmail.com>
2026-04-15 20:49:48 -0500

    Create VampireMatriarchRoleComponent.cs
    Signed-off-by: Frala <cameronfrala.personal@gmail.com>

---
+++ Content.Shared/_LateStation/Roles/Components/VampireMatriarchRoleComponent.cs
@@ -0,0 +1,14 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Roles.Components;
+
+/// <summary>
+/// Added to mind role entities to tag that they are the vampire matriarch.
+/// Tracks how many vampires this matriarch has sired.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class VampireMatriarchRoleComponent : BaseMindRoleComponent
+{
+    [DataField, AutoNetworkedField]
+    public uint SiredCount;
+}
