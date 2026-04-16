49f06a576bdbcb79cdc9560be77e68aee3609eb1
Frala <cameronfrala.personal@gmail.com>
2026-04-16 10:43:51 -0500

    Update VampireRuleComponent.cs
    Signed-off-by: Frala <cameronfrala.personal@gmail.com>

--- Content.Server/_LateStation/GameTicking/Rules/Components/VampireRuleComponent.cs
+++ Content.Server/_LateStation/GameTicking/Rules/Components/VampireRuleComponent.cs
@@ -1,24 +1,64 @@
-using System;
-using Robust.Shared.Serialization;
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Mind;
+using Content.Server.Roles;
+using Content.Shared.GameTicking.Components;
+using Content.Shared.Roles.Components;
+using Content.Shared._LateStation.Roles.Components;
+using Content.Shared._LateStation.Vampire.Components;
 
-namespace Content.Server.GameTicking.Rules.Components;
+namespace Content.Server.GameTicking.Rules;
 
 /// <summary>
-/// Stores baseline round configuration for the Vampire rule.
-/// Turning, reversion, and ratio logic will be implemented in follow-up passes.
+/// Baseline Vampire round-rule scaffold.
+/// This pass wires role briefings and activates the Matriarch body-state.
 /// </summary>
-[RegisterComponent, Access(typeof(VampireRuleSystem))]
-public sealed partial class VampireRuleComponent : Component
+public sealed class VampireRuleSystem : GameRuleSystem<VampireRuleComponent>
 {
-    [DataField]
-    public TimeSpan ConversionDelay = TimeSpan.FromSeconds(120);
+    [Dependency] private readonly MindSystem _mind = default!;
 
-    [DataField]
-    public TimeSpan MatriarchDeathReversionDelay = TimeSpan.FromSeconds(90);
+    public override void Initialize()
+    {
+        base.Initialize();
 
-    [DataField]
-    public float RequiredVampireRatio = 0.70f;
+        SubscribeLocalEvent<VampireMatriarchRoleComponent, GetBriefingEvent>(OnGetMatriarchBriefing);
+        SubscribeLocalEvent<VampireRoleComponent, GetBriefingEvent>(OnGetVampireBriefing);
+    }
 
-    [DataField]
-    public float DrawDeadCrewRatio = 0.90f;
+    private void OnGetMatriarchBriefing(Entity<VampireMatriarchRoleComponent> ent, ref GetBriefingEvent args)
+    {
+        args.Append(Loc.GetString("vamp-mat-briefing"));
+    }
+
+    private void OnGetVampireBriefing(Entity<VampireRoleComponent> ent, ref GetBriefingEvent args)
+    {
+        args.Append(Loc.GetString("vamp-briefing"));
+    }
+
+    protected override void Started(
+        EntityUid uid,
+        VampireRuleComponent component,
+        GameRuleComponent gameRule,
+        GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+
+        ActivateMatriarchBodies();
+    }
+
+    private void ActivateMatriarchBodies()
+    {
+        var query = EntityQueryEnumerator<VampireMatriarchRoleComponent>();
+
+        while (query.MoveNext(out var mindId, out _))
+        {
+            if (!_mind.TryGetMind(mindId, out _, out var mind))
+                continue;
+
+            if (mind.OwnedEntity is not { } body)
+                continue;
+
+            var vamp = EnsureComp<VampireComponent>(body);
+            vamp.CanConvert = true;
+        }
+    }
 }
